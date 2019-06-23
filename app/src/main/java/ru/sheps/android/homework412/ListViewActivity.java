ae564fd26fc02cd799de858b90b7430059c0ef4a
Katya Sheps <katya@sheps.ru>
2019-06-23 20:19:32 +0300

    Add setOnItemClickListener, SwipeRefreshLayout

--- app/src/main/java/ru/sheps/android/homework412/ListViewActivity.java
+++ app/src/main/java/ru/sheps/android/homework412/ListViewActivity.java
@@ -1,11 +1,19 @@
 package ru.sheps.android.homework412;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,24 +21,58 @@ import java.util.List;
 import java.util.Map;
 
 
-public class ListViewActivity extends AppCompatActivity {
+public class ListViewActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private static final String LARGE_TEXT = "large_text";
+    List<Map<String, String>> values = new ArrayList<>();
+    public static final String APP_PREFERENCES = "mysettings";
+    SharedPreferences mySharedPreferences;
+    BaseAdapter listContentAdapter;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
 
         ListView list = findViewById(R.id.list);
 
-        List<Map<String, String>> values = prepareContent();
+        mySharedPreferences = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        boolean hasVisited = mySharedPreferences.getBoolean("hasVisited", false);
+
+        if (!hasVisited) {
+            // выводим нужную активность
+            SharedPreferences.Editor editor = mySharedPreferences.edit();
+            editor.putBoolean("hasVisited", true);
+            editor.putString(LARGE_TEXT, getString(R.string.large_text));
+            editor.apply();
+        }
+
+        values = prepareContent();
 
         String[] from = {"Header", "Subheader"};
         int[] to = {R.id.header, R.id.subheader};
-        BaseAdapter listContentAdapter = createAdapter(values, from, to);
+        listContentAdapter = createAdapter(values, from, to);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                values.remove(position);
+                listContentAdapter.notifyDataSetChanged();
+            }
+        });
 
         list.setAdapter(listContentAdapter);
+
+        mSwipeRefreshLayout = findViewById(R.id.swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        mSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
     }
 
     private BaseAdapter createAdapter(List<Map<String, String>> values, String[] from, int[] to) {
@@ -38,17 +80,31 @@ public class ListViewActivity extends AppCompatActivity {
     }
 
     private List<Map<String, String>> prepareContent() {
-        String[] strings = getString(R.string.large_text).split("\n\n");
+
+        String[] strings = {""};
+        if (mySharedPreferences.contains(LARGE_TEXT)) {
+            strings = mySharedPreferences.getString(LARGE_TEXT, "").split("\n\n");
+        }
 
         List<Map<String, String>> list = new ArrayList<>();
 
         for (int i = 0; i < strings.length; i++) {
             Map<String, String> map = new HashMap<String, String>();
             map.put("Header", strings[i]);
-            map.put("Subheader",  String.valueOf(strings[i].length()));
+            map.put("Subheader", String.valueOf(strings[i].length()));
             list.add(map);
         }
 
         return list;
     }
+
+    @Override
+    public void onRefresh() {
+        mSwipeRefreshLayout.setRefreshing(false);
+        values.clear();
+        values.addAll(prepareContent());
+        listContentAdapter.notifyDataSetChanged();
+    }
+
+
 }
